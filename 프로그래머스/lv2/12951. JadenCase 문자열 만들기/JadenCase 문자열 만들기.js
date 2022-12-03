95ce33ff2e55e8af0876fd05cfb839ca4bcc5a14
JaneChun <108467211+JaneChun@users.noreply.github.com>
2022-12-04 01:39:35 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,18 +1,3 @@
-function solution(s) { // "for the last week"
-    const arr = s.split(' '); // ["for", "the", "last", "week"]
-    let result = [];
-    
-    
-    for(let el of arr) { // el = "for"    
-    let str = '';
-        for(let i = 0; i < el.length; i++) {
-            if (i === 0 && typeof el[i] === 'string') {
-                str = str + el[i].toUpperCase();
-            } else {
-                str = str + el[i].toLowerCase();
-            }
-        } // inner 반복문 끝
-        result.push(str);
-    } // outer 반복문 끝
-    return result.join(' ');
+function solution(s) {
+    return s.split(' ').map((str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ');
 }
