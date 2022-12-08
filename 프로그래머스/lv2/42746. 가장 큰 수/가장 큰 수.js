135adbd4d3605d59f3d0b93baa3cdf0fb1854cc6
JaneChun <108467211+JaneChun@users.noreply.github.com>
2022-12-08 19:28:29 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,6 +1,15 @@
 function solution(numbers) { // numbers = [3, 30, 34, 5, 9]
     let strings = numbers.map((num) => String(num)); // strings = ['3', '30', '34', '5', '9']
     strings.sort((a, b) => (b + a) - (a + b));       // strings = ['9', '5', '34', '3', '30']
+    // 만약 a = '3', b = '30'일 때
+    // (b + a) - (a + b) = '303' - '330' = -27로, 
+    //  330이 앞에 오고, 303이 뒤에 오는 것이 더 큰 수를 만들 수 있다.
+    
+    // 만약 a = '30', b = '34일' 때
+    // (b + a) - (a + b) = '3430' - '3034' = 396으로, 
+    // 34가 앞에 오고, 30이 뒤에 오는 것이 더 큰 수를 만들 수 있다.
+    
     const answer = strings.join(''); // '9534330'
-    return answer[0] === "0" ? "0" : answer;
+    
+    return answer[0] === "0" ? "0" : answer; // 만약 answer가 '0____'으로 시작하면 0을 리턴하도록 조건
 }
