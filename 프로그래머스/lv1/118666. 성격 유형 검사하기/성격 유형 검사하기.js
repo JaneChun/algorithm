bda53dc7761350532ea92a33545f971170128d99
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-02-10 23:38:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.51 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,28 +1,15 @@
 function solution(survey, choices) {
-    const hash = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0}
-    for (let i = 0; i < choices.length; i++) {
-        if (choices[i] < 4) {
-            const char = survey[i][0];
-            hash[char] = hash[char] + (4 - choices[i]); 
-        } else if (choices[i] > 4) {
-            const char = survey[i][1];
-            hash[char] = hash[char] + (choices[i] - 4);
-        }
-    }
+    const hash = {};
+    const types = ['RT', 'CF', 'JM', 'AN'];
     
-    const result = [];
-    const pair = ['RT', 'CF', 'JM', 'AN'];
-    for(let i = 0; i < pair.length; i++) {
-        const first = pair[i][0];
-        const second = pair[i][1];
-        const biggerOne = compare(first, hash[first], second, hash[second]);
-        result.push(biggerOne);
-    }
+    types.forEach((type) => type.split('').forEach((char) => hash[char] = 0));
     
-    return result.join('');
-}
-
-function compare(key1, val1, key2, val2) {
-    if (val1 >= val2) return key1;
-    else return key2;
+    choices.forEach((choice, i) => {
+        const [disagree, agree] = survey[i];
+        hash[choice < 4 ? disagree : agree] += Math.abs(4 - choice);
+    });
+    
+    // hash = {'R': 6, 'T': 1 ...}
+    // type = 'RT'
+    return types.map(([a, b]) => hash[a] >= hash[b] ? a : b).join('');
 }
