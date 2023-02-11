ebee23a1393efc0d25c1d0bd428be6096cbfec62
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-02-11 23:42:12 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,19 +1,7 @@
 function solution(n, arr1, arr2) {
-    const binaryArr1 = convert(arr1);
-    const binaryArr2 = convert(arr2);
-    const sumArr = binaryArr1.map((num, i) => String(num + binaryArr2[i]));
-    const result = sumArr.map((num) => {
-        let str = num.split('').map((digit) => digit > 0 ? '#' : ' ').join('');
-        if (str.length < n) {
-            while(str.length < n) {
-                str = ' ' + str;
-            }
-        }
-        return str;
-    })
-    return result;
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1/g,'#').replace(/0/g, ' '));
 }
 
-function convert(arr) {
-    return arr.map((num) => Number(num.toString(2)));
+function addZero(n, str){
+    return '0'.repeat(n - str.length) + str;
 }
