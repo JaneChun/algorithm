9f85e54711e92e80ffcd221a643ccda2bcf6e5a9
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-08 10:44:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.86 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,9 @@
+// function solution(arr) {
+//     const result = [];
+//     arr.forEach((v) => result[result.length - 1] !== v ? result.push(v) : v);
+//     return result;
+// }
+
 function solution(arr) {
     return arr.filter((v, i) => v !== arr[i - 1]);
 }
