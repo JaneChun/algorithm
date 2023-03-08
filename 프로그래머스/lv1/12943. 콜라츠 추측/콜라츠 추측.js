b9452e8e26376d37362eca72a49ecef9b3592a9e
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-08 09:59:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,10 +1,3 @@
-function solution(num) {
-    let count = 0;
-    
-    while (num !== 1 && count !== 500) {
-        num % 2 === 0 ? num = num / 2 : num = num * 3 + 1;
-        count++;
-    }
-    
-    return num === 1 ? count : -1;
+function solution(num, count = 0) {
+    return num === 1 ? (count < 500 ? count : -1) : solution(num % 2 === 0 ? num / 2 : num * 3 + 1, ++count)
  }
