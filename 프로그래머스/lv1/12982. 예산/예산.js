df30bf03a7eae41b05aa05d0a769992835b58cc0
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-15 10:20:58 +0900

    [level 1] Title: 예산, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,9 +1,9 @@
 function solution(d, budget) {
     let count = 0;
-    d.sort((a, b) => a - b);
+    d.sort((a, b) => a - b); // [1,2,3,4,5]
     for (let i = 0; i < d.length; i++) {
-        budget -= d[i];
-        count++;
+        budget -= d[i]; // 9 - 1 = 8
+        count++; // 1
         if (budget === 0) {
             break;
         } else if (budget < 0) {
