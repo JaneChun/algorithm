07771ca5efd7f976ec5213fc1dbd099ab8fecec4
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-21 11:45:29 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,20 +1,3 @@
 function solution(array, commands) {
-    let result = []; // 정답을 모아둘 배열
-    
-    for (let i = 0; i < commands.length; i++) {
-        const command = commands[i];
-        let from = command[0] - 1;
-        let to = command[1];
-        let loc = command[2] - 1;
-        
-        const slicedArr = array.slice(from, to).sort((a, b) => a - b);
-        result.push(slicedArr[loc]);
-    }
-    
-    return result;
+    return commands.map(([start, end, k]) => array.slice(start - 1, end).sort((a, b) => a - b)[k - 1]);
 }
-
-// 입력값 [1, 5, 2, 6, 3, 7, 4], [i 2, j 5, k 3] => 5
-// slicedArr = arr.slice(i - 1, j) // [5, 2, 6, 3]
-// slicedArr.sort((a, b) => a - b) // [2, 3, 5, 6]
-// return slicedArr[k - 1] // 5
