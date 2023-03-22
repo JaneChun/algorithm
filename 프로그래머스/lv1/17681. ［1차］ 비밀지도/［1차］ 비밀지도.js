d808e00230e05f3d6e4f97dc8baa8b9714bc7196
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-22 10:30:22 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,6 +1,6 @@
 function solution(n, arr1, arr2) {
     return arr1.map((v, i) => (+v.toString(2) + +arr2[i].toString(2)).toString())
             .map((v) => v.length < n ? '0'.repeat(n - v.length) + v : v) // 부족한 자리수만큼 앞에 0 붙이기
-            .map((v) => v.split('').map((v) => v === '0' ? ' ' : '#').join('')) // 0이면 ' ', 아니면 '#'으로 매핑하기  
+            .map((v) => v.split('').map((v) => v === '0' ? ' ' : '#').join('')) // 0이면 ' ', 아니면 '#'으로 매핑하기  ["0","0","2","2","0"]
     
 }
