f4c2683851779f21249fa659eeafee5c0283e2e3
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-03-27 10:06:59 +0900

    [unrated] Title: 콜라 문제, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,10 +1,10 @@
 function solution(a, b, n) {
     let received = 0;
     while (n >= a) {
-        let give = Math.floor(n / a);
-        n -= a * give;
-        n += give * b;
-        received += give * b;
+        let give = Math.floor(n / a); // 10묶음
+        n -= a * give; // 20 - 10 * 2 = 0
+        n += give * b; // 0 + 10 * 1 = 10
+        received += give * b; // 10
     }
     return received;
 }
