4da1d698207d5406afded5e2eed81d6b7ad1a3ce
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-04-03 18:29:07 +0900

    [level 1] Title: 소수 찾기, Time: 67.73 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,14 +1,17 @@
 function solution(n) {
-    const sieve = new Array(n + 1).fill(true);
+    const sieve = [];
+    
+    for (let k = 2; k <= n; k++) {
+        sieve[k] = k;
+    }
     
     for (let i = 2; i <= n; i++) {
-        if (sieve[i] === false) continue;  // 이미 지워진 숫자라면 무시
+        if (sieve[i] === false) continue;
         
-        // 이미 지워진 숫자가 아니라면
-        for (let j = i + i; j <= n; j += i) { // 본인 제외 배수부터 시작해서 다 지워준다.
+        for (let j = i + i; j <= n; j += i) {
             sieve[j] = false;
         }
     }
     
-    return sieve.filter((el) => el).length - 2;
+    return sieve.filter((el) => el).length;
 }
