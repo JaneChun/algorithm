01dd108666b6004f6cff041d585829340060f215
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-04-10 10:09:58 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -4,19 +4,19 @@ function solution(k, score) {
     let lowest = 0;
     
     for (let i = 0; i < score.length; i++) {
-        if (i < k) {
-            hallOfFame.push(score[i]);
-            hallOfFame.sort((a, b) => a - b);
-            lowest = hallOfFame[0]
-            lowestArr.push(lowest);
-        } else {
+        if (i < k) { // i가 k번째보다 작을 경우
+            hallOfFame.push(score[i]); // 명예의 전당에 넣기
+            hallOfFame.sort((a, b) => a - b); // 정렬
+            lowest = hallOfFame[0] // 최저값 0번째
+            lowestArr.push(lowest); // 
+        } else { // i > k번째
             if (score[i] > lowest) {
-                hallOfFame.shift();
-                hallOfFame.push(score[i]);
-                hallOfFame.sort((a, b) => a - b);
+                hallOfFame.shift(); // k번째 방출
+                hallOfFame.push(score[i]); // score[i] 넣기
+                hallOfFame.sort((a, b) => a - b); // 정렬
             }
-            lowest = hallOfFame[0]
-            lowestArr.push(lowest);
+            lowest = hallOfFame[0] // 최저값 0번째
+            lowestArr.push(lowest); //
         }
     }
     
