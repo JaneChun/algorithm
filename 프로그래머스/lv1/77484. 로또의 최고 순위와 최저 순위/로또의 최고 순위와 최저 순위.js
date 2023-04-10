841f32954967d5094e3eba2f255263bba38619cb
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-04-10 10:18:42 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,6 +1,6 @@
 function solution(lottos, win_nums) {
-    let zeroCount = 0;
-    let winCount = 0;
+    let zeroCount = 0; // 0의 개수
+    let winCount = 0; // 맞춘 번호 개수
     const ranks = {0: 6, 1: 6, 2: 5, 3: 4, 4: 3, 5: 2, 6: 1}
     
     for (let i = 0; i < lottos.length; i++) {
@@ -10,4 +10,4 @@ function solution(lottos, win_nums) {
         }
     }
     return [ranks[winCount + zeroCount], ranks[winCount]];
-}
+} // 최저 등수 : winCount / 최고 등수 : zero + win
