f2a6b78668544afcdca92c14851756e1e44ebde1
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-04-17 03:45:39 +0900

    [unrated] Title: 옹알이 (2), Time: 0.28 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,21 @@
+function solution(babbling) {
+    let count = 0;
+    
+    babbling.forEach((string) => { // 'ayaye'
+        const regex = /(aya|ye|woo|ma)/g;
+        const matches = string.match(regex); // ['aya', 'ye']
+        
+        if (!matches)  return;
+        
+        let prev = ''
+        matches.forEach((match) => { // 'aya'
+            if (prev === match) return;
+            string = string.replace(match, ''); // 문자열에서 match 잘라내기
+            prev = match;
+        });
+
+        if (string === '') count++;        
+    })
+    
+    return count;
+}
