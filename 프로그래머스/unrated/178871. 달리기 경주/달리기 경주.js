0de592b77be1fb9ecee453e440fa4977cfa4fdfe
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-01 12:33:46 +0900

    [unrated] Title: 달리기 경주, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -1,10 +1,10 @@
 function solution(players, callings) {
-    const hash = {};
-    players.forEach((player, i) => hash[player] = i);
+    const map = new Map();
+    players.forEach((player, i) => map.set(player, i));
     // { mumu: 0, soe: 1, poe: 2, kai: 3, mine: 4 }
     
     callings.forEach((player) => {
-        const prevRanking = hash[player]; // 3
+        const prevRanking = map.get(player); // 3
         const passedPlayer = players[prevRanking - 1]; // 추월당한 선수
         
         // 배열 업데이트
@@ -12,8 +12,8 @@ function solution(players, callings) {
         players[prevRanking - 1] = player;
         
         // hash 업데이트
-        hash[passedPlayer] = prevRanking;
-        hash[player] = prevRanking - 1;
+        map.set(passedPlayer, prevRanking);
+        map.set(player, prevRanking - 1);
     })
     return players;
 }
