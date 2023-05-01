030fc9fce6e259bac0209121e4c4daae33951289
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-01 10:23:29 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.57 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -13,5 +13,3 @@ function solution(today, terms, privacies) {
     
     return result;
 }
-
-// solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
