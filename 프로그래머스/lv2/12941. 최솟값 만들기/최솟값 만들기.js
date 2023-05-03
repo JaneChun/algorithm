78d87970ad98a1bb14d017366faa7b991635affc
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-03 11:00:22 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.81 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,14 +1,6 @@
-function solution(A, B){
-// A = [1, 4, 2] , B = [5, 4, 4]
-// 두 배열을 sort 하면
-// [1, 2, 4], [4, 5, 5] = 4 + 10 + 20
-// 순서대로 곱하기
-    let result = 0;
+function solution(A,B){
     A.sort((a, b) => a - b);
     B.sort((a, b) => b - a);
-    
-    for(let i = 0; i < A.length; i++) {
-       result += A[i] * B[i];
-   }
-    return result;
+
+    return A.reduce((acc, cur, i) => acc + cur * B[i],0);
 }
