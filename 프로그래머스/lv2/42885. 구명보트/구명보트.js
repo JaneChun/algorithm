d8eca155edaa74c6addc20a88f666ad553b8b6d7
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-30 10:35:44 +0900

    [level 2] Title: 구명보트, Time: 10.85 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,10 +1,10 @@
 function solution(people, limit) {
-    people.sort((a, b) => a - b);
+    people.sort((a, b) => a - b); // 50 70
     let boats = 0;
     
     while(people.length > 0) {
-        const heaviest = people.pop();
-        const lightest = people[0];
+        const heaviest = people.pop(); // 80
+        const lightest = people[0]; // 50
         
         if (lightest + heaviest <= limit) {
             people.shift();
