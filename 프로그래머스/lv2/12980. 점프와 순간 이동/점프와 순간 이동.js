12b8c32e03b3d129431641bc07b2405e05d3ad44
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-30 10:45:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -1,12 +1,18 @@
 function solution(n) {
     let jump = 0;
+    
     while (n > 0) {
-        if (n % 2 === 0) {
+        if (n % 2 === 0) { // 순간이동 할 수 있는 경우
             n = n / 2;
         } else {
-            n -= 1;
-            jump++;
+            n -= 1; // 점프 1칸
+            jump++; // 점프 횟수
         }
     }
     return jump;
 }
+
+// 점프를 하면 1칸당 건전지 1 사용
+// 순간이동은 지금까지 온 거리 * 2
+
+// n -> n / 2
