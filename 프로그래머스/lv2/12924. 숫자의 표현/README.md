76cddd6a503090f20df2321520646cbd6e8e5ad3
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-30 22:57:57 +0900

    [level 2] Title: 숫자의 표현, Time: 1.51 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/README.md
+++ 프로그래머스/lv2/12924. 숫자의 표현/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자의 표현 - 12924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12924#qna) 
 
 ### 성능 요약
 
-메모리: 36.2 MB, 시간: 1.44 ms
+메모리: 36.3 MB, 시간: 1.51 ms
 
 ### 구분
 
--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,16 +1,20 @@
 function solution(n) {
-    let result = 0;
-    for (let i = 1; i <= n; i++) {
-        let sum = i;
-        for(let j = i + 1; j <= n; j++) {
-            sum += j;
-            if (sum === n) {
-                result++;
-                break;
-            } else if (sum > n) {
-                break;
-            }
+    let count = 0;
+    let sum = 1;
+    let left = 1;
+    let right = 2;
+    while (left <= n) {
+        if (sum === n) {
+            count++;
+            sum -= left;
+            left++;
+        } else  if (sum < n) {
+            sum += right;
+            right++;
+        } else if (sum > n) {
+            sum -= left;
+            left++;
         }
     }
-    return result;
+    return count;
 }
