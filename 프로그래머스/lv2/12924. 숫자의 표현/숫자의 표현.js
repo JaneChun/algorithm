b1b85bef34f53e8c80c2b13645de6d18c547c1dd
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-30 22:41:31 +0900

    [level 2] Title: 숫자의 표현, Time: 1.44 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,8 @@
 function solution(n) {
     let result = 0;
-    
     for (let i = 1; i <= n; i++) {
-        let sum = 0;
-        for(let j = i; j <= n; j++) {
+        let sum = i;
+        for(let j = i + 1; j <= n; j++) {
             sum += j;
             if (sum === n) {
                 result++;
