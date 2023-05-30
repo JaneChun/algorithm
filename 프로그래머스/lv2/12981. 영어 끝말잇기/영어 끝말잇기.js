e59cc00eea042c1b67fa51973b14de165fb8915f
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-05-30 10:32:28 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.33 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -4,10 +4,10 @@ function solution(n, words) {
     for (let i = 0; i < words.length; i++) {
         const word = words[i];
         const prev = words[i - 1];
+        const number = (i + 1) % n === 0 ? n : (i + 1) % n;
+        const turn = Math.ceil((i + 1) / n);
         
-        if ((i > 0 && word[0] !== prev[prev.length - 1]) || hash[word]) {
-            const number = (i + 1) % n === 0 ? n : (i + 1) % n;
-            const turn = Math.ceil((i + 1) / n);
+        if ((i > 0 && word[0] !== prev[prev.length - 1]) || hash[word]) {  
             return [number, turn];
         } else { // hash[word] === undefined
             hash[word] = true;
