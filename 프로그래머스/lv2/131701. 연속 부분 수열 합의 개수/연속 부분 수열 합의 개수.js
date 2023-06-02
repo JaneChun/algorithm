f292073b4159e3a6a7f13f4dc0102df3a9af23cb
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-06-02 10:32:47 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 64.89 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,17 +1,18 @@
 function solution(elements) {
     const sumSet = new Set();
+    const len = elements.length;
 
-    for (let w = 1; w <= elements.length; w++) { // w는 window의 크기
+    for (let w = 1; w <= len; w++) { // w는 window의 크기 // w = 1, 2, 3, 4, 5
         let windowSum = 0;
         
-        for (let j = 0; j < elements.length; j++) { // 배열 elements를 순회 (j는 창문 시작 인덱스)
-            if (j === 0) { // 맨 처음의 창문에 대해서만 직접 합을 구한다.
-                for (let k = 0; k < w; k++) { // 창문 안에 있는 요소들을 (w개)
-                    windowSum += elements[k]; // windowSum에 더해준다.
+        for (let i = 0; i < len; i++) { // 배열 순회 i = 0, 1, 2, 3, 4
+            if (i === 0) { // 첫번째만 계산해줌
+                for (let j = 0; j < w; j++) { // 창문 크기만큼 반복문으로 더하기
+                    windowSum += elements[j];
                 }
-            } else { // 이후 창문들은 이전에 계산한 값을 활용한다.
-                windowSum -= elements[j - 1]; // 이전 값 빼주기
-                windowSum += elements[(j + w - 1) % elements.length]; // 다음 값 더해주기, 원형 수열이므로 % 이용
+            } else { // 그 뒤의 경우에는
+                windowSum -= elements[i - 1];
+                windowSum += elements[(i + w - 1) % len];
             }
             sumSet.add(windowSum);
         }
