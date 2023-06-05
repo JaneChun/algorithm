7c0ecaf3edba86d9f97528b395e4be04c1180101
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-06-05 10:07:56 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,19 +1,18 @@
-function solution(brown, yellow) {
+function solution(brown, yellow) { // 10, 2
     const answer = [0, 0];
-    const S = brown + yellow;
-
+    const S = brown + yellow; // 전체 면적 S = 12
     
-    for (let width = S; width > 0; width--) {
-        if (S % width !== 0) continue;
+    for (let width = S; width > 0; width--) { // width = 4
+        if (S % width !== 0) continue; // 전체 면적이 너비로 나누어떨어지지 않는 경우는 스킵
         
-        const height = S / width;
-        const yellowS = (width - 2) * (height - 2);
-        const brownS = S - yellowS;
+        const height = S / width;                   // height = 3
+        const yellowS = (width - 2) * (height - 2); // 2 * 1 = 2
+        const brownS = S - yellowS;                 // 10
         
-        if (yellowS === yellow && brownS === brown) {
+        if (yellowS === yellow && brownS === brown) { // 조건이 맞아 떨어지면
             answer[0] = width;
             answer[1] = height;
-            break;
+            break; // 반복문 탈출
         }
     }
     
