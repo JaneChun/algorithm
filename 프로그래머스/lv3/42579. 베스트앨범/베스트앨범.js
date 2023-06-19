bbc3640c7c53156ac104a3ed95b9f3f1135c0079
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-06-19 09:31:28 +0900

    [level 3] Title: 베스트앨범, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -1,20 +1,37 @@
 function solution(genres, plays) {
     const hash = {};
     
-    genres
-        .map((genre, i) => [genre, plays[i]])
-        .forEach(([genre, play], index) => {
-        const data = hash[genre] || { total: 0, songs: [] };
-        hash[genre] = {
-            total: data.total + play, 
-            songs: [...data.songs, {play, index}]
-                        .sort((a, b) => b.play - a.play)
-                        .slice(0, 2)
+    genres.map((genre, i) => [genre, plays[i]])
+    // [
+    //   [ 'classic', 500 ],
+    //   [ 'pop', 600 ],
+    //   [ 'classic', 150 ],
+    //   [ 'classic', 800 ],
+    //   [ 'pop', 2500 ]
+    // ]
+          .forEach(([genre, play], index) => {
+            const data = hash[genre] || { total: 0, songs: [] }; // hash에 저장되어있으면 불러오고, 없으면 {total: 0, songs: []}로 값 추가
+            
+            hash[genre] = {
+                total: data.total + play, // total에 재생수 더하기
+                songs: [...data.songs, {play, index}] // songs 배열에 현재 곡을 {play: 500, index: 0}의 형태로 저장하기
+                        .sort((a, b) => b.play - a.play) // 재생수 많은 순으로 내림차순 정렬
+                        .slice(0, 2) // 베스트 2개만 뽑기
             }
     })
+    // console.log(hash)
+    // {
+    //   classic: { total: 1450, songs: [  { play: 800, index: 3 }, { play: 500, index: 0 } ] },
+    //   pop: { total: 3100, songs: [ { play: 2500, index: 4 }, { play: 600, index: 1 } ] }
+    // }
     
-    return Object.entries(hash)
-        .sort((a, b) => b[1].total - a[1].total)
-        .flatMap((item) => item[1].songs)
-        .map((song) => song.index)
+    // console.log(Object.entries(hash))
+    // [
+    //   [ 'classic', { total: 1450, songs: [Array] } ],
+    //   [ 'pop', { total: 3100, songs: [Array] } ]
+    // ]
+    
+    return Object.entries(hash).sort((a, b) => b[1].total - a[1].total)
+                               .flatMap((el) => el[1].songs) // [{ play: 2500, index: 4 }, { play: 600, index: 1 }, { play: 800, index: 3 }, { play: 500, index: 0 }]
+                               .map((song) => song.index);
 }
