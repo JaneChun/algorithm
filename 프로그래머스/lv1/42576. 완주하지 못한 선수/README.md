f695b6f71e66a2d49c68e21bc7547a9048628424
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-06-19 10:09:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 50.51 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 56.9 MB, 시간: 103.40 ms
+메모리: 63.4 MB, 시간: 50.51 ms
 
 ### 구분
 
--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,10 @@
 function solution(participant, completion) {
-    participant.sort();
-    completion.sort();
-
-    for (let i = 0; i < participant.length; i++) {
-        if (participant[i] !== completion[i]) {
-            return participant[i];
-        }
+    const map = new Map();
+    
+    participant.forEach((name) => map.set(name, (map.get(name) || 0) + 1));
+    completion.forEach((name) => map.set(name, (map.get(name) || 0) - 1));
+    
+    for(const [key, value] of map) {
+        if (value > 0) return key;
     }
-    return participant[participant.length - 1];
 }
