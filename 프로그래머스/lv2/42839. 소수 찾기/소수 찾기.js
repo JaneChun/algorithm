eedbef7510ef17a3a64e38d80ae2476a9902409f
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-06-28 10:13:15 +0900

    [level 2] Title: 소수 찾기, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,29 +1,50 @@
 function solution(numbers) {
+    const perArr = getPermutations(numbers.split(''));
+    
     const set = new Set();
-    dfs('', [...numbers], set);
-    return set.size;
+    perArr.map((arr) => set.add(Number(arr.join(''))));
+    console.log(set); // { 17, 71 }
+    
+    let countOfPrimeNumbers = 0;
+    for (const num of set) {
+        // 소수면
+        console.log(num, isPrimeNumber(num))
+        if (isPrimeNumber(num)) countOfPrimeNumbers++;
+    }
+    return countOfPrimeNumbers;
 }
 
-function isPrime(num) {
-    if (num <= 1) return false;
-    if (num === 2) return true;
-    if (num % 2 === 0) return false;
+function isPrimeNumber(num) {
+    if (num <= 1) return false; // 0, 1은 소수가 아님
+    if (num === 2) return true; // 2는 소수임
+    if (num % 2 === 0) return false; // 짝수는 소수가 아님
     
     for (let i = 3; i <= Math.floor(Math.sqrt(num)); i += 2) {
-        if (num % i === 0) return false;
+        if (num % i === 0) {
+            return false;
+        }
+        return true;
     }
     return true;
 }
 
-function dfs(fixed, rest, set) {
-    if (rest.length >= 1) {
-        for (let i = 0; i < rest.length; i++) {
-            let newFixed = fixed + rest[i];
-            let copyArr = [...rest];
-            copyArr.splice(i, 1);
-            if (isPrime(+newFixed)) set.add(+newFixed);
-            
-            dfs(newFixed, copyArr, set);
+function getPermutations(arr) {
+    const result = [];
+    arr.forEach((num) => result.push([num])); // 요소 하나로 이루어진 숫자도 추가해야하므로
+    
+    const dfs = (i, arr) => {
+        if (i === arr.length) {
+            return result.push([...arr]);
+        }
+
+        for (let j = i; j < arr.length; j++) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            dfs(i + 1, arr);
+            [arr[i], arr[j]] = [arr[j], arr[i]];
         }
     }
+    
+    dfs(0, arr);
+    
+    return result;
 }
