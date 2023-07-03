83ec382b1dcb1ccf3db090b883b1befd0f93a370
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-07-03 10:11:35 +0900

    [level 2] Title: 소수 찾기, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/README.md
+++ 프로그래머스/lv2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.36 ms
+메모리: 33.5 MB, 시간: 0.34 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.js
@@ -1,50 +1,47 @@
 function solution(numbers) {
-    const perArr = getPermutations(numbers.split(''));
-    
     const set = new Set();
-    perArr.map((arr) => set.add(Number(arr.join(''))));
-    console.log(set); // { 17, 71 }
-    
-    let countOfPrimeNumbers = 0;
-    for (const num of set) {
-        // 소수면
-        console.log(num, isPrimeNumber(num))
-        if (isPrimeNumber(num)) countOfPrimeNumbers++;
-    }
-    return countOfPrimeNumbers;
+    dfs('', [...numbers], set);
+    return set.size;
 }
 
-function isPrimeNumber(num) {
+// fixed : 고정된 문자열
+// rest : 고정된 문자열을 제외한 값이 담긴 배열
+// set : 값을 저장할 Set 객체
+function dfs(fixed, rest, set) { // '1', [2, 3], set
+    if (rest.length >= 1) {
+        for (let i = 0; i < rest.length; i++) { // i = 1
+            let newFixed = fixed + rest[i]; // newFixed = '13'
+            let copyArr = [...rest]; // [2]
+            copyArr.splice(i, 1);
+            if (isPrime(+newFixed)) set.add(+newFixed); // set에 넣을 때는 숫자
+            
+            dfs(newFixed, copyArr, set); // 1, [2, 3], []
+        }
+    }
+}
+
+// dfs([1, 2, 3], '')을 호출하면 dfs가 호출되는 순서
+
+// dfs('', [1, 2, 3])
+//  ㄴ dfs('1', [2, 3])     -> fixed = '',   i = 0일 때 (1 depth)
+//     ㄴ dfs('12', [3])    -> fixed = '1',  i = 0일 때 (2 depth)
+//        ㄴ dfs('123', []) -> fixed = '12', i = 0일 때 (3 depth)
+//     ㄴ dfs('13', [2])    -> fixed = '1',  i = 1일 때 (2 depth)
+//        ㄴ dfs('132', []) -> fixed = '13', i = 0일 때 (3 depth)
+//  ㄴ dfs('2', [1, 3])     -> fixed = '',   i = 1일 때 (1 depth)
+//      ㄴ dfs('21', [3])   -> fixed = '2',  i = 0일 때 (2 depth)
+//        ㄴ dfs('213', []) -> fixed = '21', i = 0일 때 (3 depth)
+//      ㄴ dfs('23', [1])   -> fixed = '2',  i = 1일 때 (2 depth)
+//        ㄴ dfs('231', []) -> fixed = '23', i = 0일 때 (3 depth)
+// ...
+
+function isPrime(num) {
     if (num <= 1) return false; // 0, 1은 소수가 아님
     if (num === 2) return true; // 2는 소수임
     if (num % 2 === 0) return false; // 짝수는 소수가 아님
     
     for (let i = 3; i <= Math.floor(Math.sqrt(num)); i += 2) {
-        if (num % i === 0) {
-            return false;
-        }
-        return true;
+        if (num % i === 0) return false;
     }
     return true;
-}
-
-function getPermutations(arr) {
-    const result = [];
-    arr.forEach((num) => result.push([num])); // 요소 하나로 이루어진 숫자도 추가해야하므로
-    
-    const dfs = (i, arr) => {
-        if (i === arr.length) {
-            return result.push([...arr]);
-        }
-
-        for (let j = i; j < arr.length; j++) {
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-            dfs(i + 1, arr);
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-    }
-    
-    dfs(0, arr);
-    
-    return result;
 }
