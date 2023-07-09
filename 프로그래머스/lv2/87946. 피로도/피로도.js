da05af31966a8fd39d00bb793ea1f68310abda8b
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-07-10 08:41:48 +0900

    [level 2] Title: 피로도, Time: 0.33 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.js
+++ 프로그래머스/lv2/87946. 피로도/피로도.js
@@ -1,21 +1,25 @@
 function solution(k, dungeons) {
-    let count = 0;
+    let max = 0;
     const visited = Array.from({length: dungeons.length}, () => false);
     
-    function dfs(hp, depth) {
-        if (count < depth) count = depth;
-        
+    function dfs(hp, completed) {
         for (let i = 0; i < dungeons.length; i++) {
             const [need, consume] = dungeons[i];
             if (!visited[i] && hp >= need) {
                 visited[i] = true;
-                dfs(hp - consume, depth + 1);
+                dfs(hp - consume, completed + 1);
                 visited[i] = false;
             }
         }
+        
+        max = Math.max(max, completed); // 최대 던전 클리어횟수 업데이트
     }
     
     dfs(k, 0);
     
-    return count;
+    return max;
 }
+
+//        [80, 20]                [50, 40]                [30, 10]
+//  [50, 40]    [30, 10]    [80, 20]    [30, 10]     [80, 20]    [50, 40] 
+//  [30, 10]    [50, 40]    [30, 10]    [80, 20]     [50, 40]    [80, 20] 
