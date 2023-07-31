2ef5f1cf4e0f96dbb0b28adaab3f72fcca446c63
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-07-31 10:31:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 57.51 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -6,21 +6,31 @@ function solution(n, edge) {
         graph[b].push(a);
     });
     
-    const distance =  Array(n + 1).fill(0);
+    const distance = Array(n + 1).fill(0); // [0, 1, 2, 2, 0, 0, 0]
     distance[1] = 1;
     
     const queue = [];
-    queue.push(1);
+    queue.push(1); // queue = [4 6 5]
     while (queue.length) {
-        const from = queue.shift();
-        for (const dest  of graph[from]) {
-            if (distance[dest] === 0) {
-                queue.push(dest);
+        const from = queue.shift(); // from = 1
+        for (const dest  of graph[from]) { // graph[1] = [3, 2] // dest = 3, dest = 2
+            if (distance[dest] === 0) { // 방문 X
+                queue.push(dest); // [3]
                 distance[dest] = distance[from] + 1;
             }
         }
     }
-    
     const max = Math.max(...distance);
     return distance.filter((d) => d === max).length;
 }
+  // graph
+  //   	[
+  // [],
+  // [ 3, 2 ],
+  // [ 3, 1, 4, 5 ],
+  // [ 6, 4, 2, 1 ],
+  // [ 3, 2 ],
+  // [ 2 ],
+  // [ 3 ],
+  // []
+  //   ]
