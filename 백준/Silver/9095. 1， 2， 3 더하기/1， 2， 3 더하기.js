d4029d45cde3e3f35eaf19c9cd9b89ac45af63a1
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-08-13 23:52:09 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+const arr = input.slice(1);
+const answer = [];
+
+for (const N of arr) {
+	let count = 0;
+	const dfs = (target, sum) => {
+		if (sum === target) count++;
+		if (sum < target) {
+			dfs(target, sum + 1);
+			dfs(target, sum + 2);
+			dfs(target, sum + 3);
+		}
+	};
+
+	dfs(N, 0);
+
+	answer.push(count);
+}
+
+console.log(answer.join('\n'));
