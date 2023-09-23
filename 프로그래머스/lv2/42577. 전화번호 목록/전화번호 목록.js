4aac0d513a842ac728b6fc0b115f7e0077be09b9
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-09-23 20:16:17 +0900

    [level 2] Title: 전화번호 목록, Time: 115.24 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,14 +1,7 @@
 function solution(phone_book) {
-    const hash = {};
-    phone_book.forEach((v) => hash[v] = true);
-    
-    for (const number of phone_book) {
-        for (let i = 0; i < number.length; i++) {
-            const sliced = number.slice(0, i + 1)
-            hash[number] = false;
-            if (hash[sliced]) return false;
-            hash[number] = true;
-        }
-    }
-    return true;
+    return !phone_book.sort().some((_, i) => {
+        if (i === phone_book.length - 1) return false;
+        
+        return phone_book[i+1].startsWith(phone_book[i]);
+    })
 }
