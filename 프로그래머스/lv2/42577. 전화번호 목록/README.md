5dc0c117c7c38c848be532f70510e431a7ae7f18
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-09-24 10:22:03 +0900

    [level 2] Title: 전화번호 목록, Time: 487.48 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/README.md
+++ 프로그래머스/lv2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 115.24 ms
+메모리: 126 MB, 시간: 487.48 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,7 +1,22 @@
 function solution(phone_book) {
-    return !phone_book.sort().some((_, i) => {
-        if (i === phone_book.length - 1) return false;
-        
-        return phone_book[i+1].startsWith(phone_book[i]);
-    })
+    const hash = {};
+    phone_book.forEach((v) => hash[v] = true);
+
+    for (const number of phone_book) {
+        for (let i = 0; i < number.length; i++) {
+            const sliced = number.slice(0, i + 1)
+            hash[number] = false;
+            if (hash[sliced]) return false;
+            hash[number] = true;
+        }
+    }
+    return true;
 }
+
+// function solution(phone_book) {
+//     return !phone_book.sort().some((_, i) => {
+//         if (i === phone_book.length - 1) return false;
+        
+//         return phone_book[i+1].startsWith(phone_book[i]);
+//     })
+// }
