c4cfa29ff56fba54240d88925586e861b2d88020
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-10-15 00:54:44 +0900

    [level 2] Title: 땅따먹기, Time: 48.27 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -0,0 +1,10 @@
+function solution(land) {
+    for (let i = 1; i < land.length; i++) {
+        for (let j = 0; j < 4; j++) {
+            // 같은 인덱스는 제외한 이전 land 배열
+            const prevLand = land[i - 1].filter((_, _i) => j !== _i)
+            land[i][j] += Math.max(...prevLand)
+        }
+    }
+    return Math.max(...land.pop())
+}
