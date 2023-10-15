d463bd380f8b1ad8913e84a87da243dc78f3e23a
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-10-15 10:36:19 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.50 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 3.44 ms
+메모리: 33.6 MB, 시간: 0.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 0일 1:27:33
+2023년 10월 0일 10:36:18
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -5,28 +5,18 @@ function solution(wallpaper) {
     //   [ 0, 1, 0, 0, 0 ], 
     //   [ 0, 0, 1, 0, 0 ], 
     //   [ 0, 0, 0, 1, 0 ] 
-    //]
-    
-// [
-//   [0, 0, 0, 0, 0,0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
-//   [0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-// ]
-    
+    // ]
+
     // 가장 max 왼쪽 값, max 위쪽 값, max 오른쪽 값, max 아래 값을 찾아야 한다. => S(위, 왼), E(아래, 오)
     let left = colLen - 1, up = rowLen - 1, right = 0, down = 0;
     for (let row = 0; row < rowLen; row++) {
         for (col = 0; col < colLen; col++) {
             const value = matrix[row][col]
-            if (value) {
-                console.log('row, col', row, col)
+            if (value) { // 1
                 up = Math.min(up, row)
                 left = Math.min(left, col)
                 down = Math.max(down, row + 1)
-                right = Math.max(right, col + 1) // 최대값이 바탕화면을 넘어갈 때는 + 1 하지 않는다.
-                console.log('[위, 왼, 아래, 오]', [up, left, down, right])
+                right = Math.max(right, col + 1)
             }
         }
     }
