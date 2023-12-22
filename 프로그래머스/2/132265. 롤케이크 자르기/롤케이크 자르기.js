cf0a72a98d732c509ffca9248cfd8fcdda182a5b
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-12-22 09:33:08 +0900

    [level 2] Title: 롤케이크 자르기, Time: 116.92 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -0,0 +1,24 @@
+function solution(toppings) {
+    let count = 0
+    
+    const allTopping = new Map()
+    const bro = new Set()
+    
+    toppings.forEach((topping) => allTopping.set(topping, (allTopping.get(topping) || 0) + 1))
+    
+    console.log(allTopping)
+    // { 1 => 4, 2 => 2, 3 => 1, 4 => 1 }
+    
+    toppings.forEach((topping) => {
+        allTopping.set(topping, allTopping.get(topping) - 1) // 전체 토핑에서 하나 빼서
+        bro.add(topping) // 형 토핑에 추가
+        
+        if (allTopping.get(topping) === 0) {
+            allTopping.delete(topping)
+        }
+        
+        if (allTopping.size === bro.size) count++
+    })
+    
+    return count
+}
