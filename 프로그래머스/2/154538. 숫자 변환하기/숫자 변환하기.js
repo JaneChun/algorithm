d6a2359b8dbcb00389637d3a688a0aab0d2067fc
JaneChun <108467211+JaneChun@users.noreply.github.com>
2023-12-31 10:41:38 +0900

    [level 2] Title: 숫자 변환하기, Time: 37.06 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -1,12 +1,15 @@
 function solution(x, y, n) {
     const dp = Array(y + 1).fill(Infinity)
     dp[x] = 0 // x에서 i를 만들 수 있는 최소 연산횟수를 dp[i]에 저장
+    // dp[x] = 0 // x = 10 //  dp[10] = 0
+    // dp[20] = 1, dp[30] = 1
+    // dp[40] = 2
     // x부터 y까지 돌면서 dp[i]를 업데이트한다.
     
     for (let i = x; i <= y; i++) {
-        dp[i + n] = Math.min(dp[i + n], dp[i] + 1)
-        dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1)
-        dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1)
+        if (dp[i + n]) dp[i + n] = Math.min(dp[i + n], dp[i] + 1)
+        if (dp[i * 2]) dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1)
+        if (dp[i * 3]) dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1)
     }
     
     return dp[y] === Infinity ? -1 : dp[y]
