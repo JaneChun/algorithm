41c960e4e389d6fabeb0ae67c4a6ba1946fba562
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-03-30 21:42:15 +0900

    [level 2] Title: 후보키, Time: 5.25 ms, Memory: 34.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.js
@@ -0,0 +1,65 @@
+// 조합 생성 함수
+function makeCombinations (arr, level, start = 0, prefix = 0) {
+    if (level === 1) return arr.map((v) => [v])
+    
+    const result = []
+    arr.forEach((fixed, i, origin) => {
+        const rest = origin.slice(i + 1) // fixed를 제외한 나머지에 대해서 조합을 구한다.
+        const combinations = makeCombinations(rest, level - 1) 
+
+        const attached = combinations.map(v => [fixed, ...v]) // 돌아온 조합에 떼놓은 값(fixed)를 붙인다.
+        result.push(...attached)
+    });
+    return result
+}
+
+// 유일성 체크 함수
+function checkUniqueness(arr) {
+    const set = new Set()
+    for (const item of arr) {
+        if (set.has(item)) return false // 중복이 발생하면 유일성을 만족하지 않음
+        set.add(item)
+    }
+    return true // 중복이 없으면 유일성을 만족함
+}
+
+// 최소성 체크 함수
+function checkMinimality(idxArray, candidates) { // [[0]] 후보키 배열을 순회하며
+    for (const candidate of candidates) { // [0] 후보키 배열의 모든 요소가 입력된 idxArray에 포함되는지 확인
+        for (let i = 0; i < candidate.length; i++) {
+            if (candidate.every(key => idxArray.includes(key))) { // 최소성 만족 X
+                return false
+            }
+        }
+    }
+    return true // 최소성 만족
+}
+
+function solution(relation) {
+    let count = 0
+    
+    const columnIdxArray = Array.from({length: relation[0].length}, (_, i) => i) // [ 0, 1, 2, 3 ]
+    const columnIdxCombinations = columnIdxArray.reduce((acc, _, i) => {
+        acc.push(...makeCombinations(columnIdxArray, i + 1))
+        return acc
+    }, [])
+    console.log(columnIdxCombinations)
+    // [
+    //   [ 0 ], [ 1 ], [ 2 ], [ 3 ],
+    //   [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ],
+    //   [ 0, 1, 2 ], [ 0, 1, 3 ], [ 0, 2, 3 ], [ 1, 2, 3 ],
+    //   [ 0, 1, 2, 3 ]
+    // ]
+    
+    const candidates = [] // 후보키
+    
+    columnIdxCombinations.forEach((idxArray) => { // [0, 1]
+        const arr = relation.map((row) => idxArray.map((idx) => row[idx]).join(''))
+        if (checkUniqueness(arr) && checkMinimality(idxArray, candidates)) {
+            candidates.push(idxArray)
+            count++
+        }
+    })
+    
+    return count
+}
