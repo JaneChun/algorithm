76524c4e993ab0bef45d1c97e8ca032f32861fdc
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-05-12 00:22:16 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,5 +1,7 @@
 from functools import reduce
 
 def solution(num_str):
-    arr = [int(char) for char in num_str];
-    return reduce(lambda acc, cur: acc + cur,arr)
+    return sum(map(int, num_str))
+
+    # arr = [int(char) for char in num_str];
+    # return reduce(lambda acc, cur: acc + cur,arr)
