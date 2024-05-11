4f675bc75a55f4739336039ba47ef63ddc170053
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-05-11 23:54:10 +0900

    [level 0] Title: 배열의 유사도, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,7 +1,8 @@
 def solution(s1, s2):
-    answer = 0
-    for char in s1:
-        if char in s2:
-            answer = answer + 1
+    return len(set(s1)&set(s2));
+#     answer = 0
+#     for char in s1:
+#         if char in s2:
+#             answer = answer + 1
             
-    return answer
+#     return answer
