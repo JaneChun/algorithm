8f633e63be1df7d3835d2f9faef85273242d582a
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-06-08 23:49:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,3 +1,5 @@
 def solution(n):
-    repeated = "수박" * int(n/2)
-    return repeated if n % 2 == 0 else repeated + "수"
+    return "수박" * (n//2) + "수" * (n % 2)
+    
+    # repeated = "수박" * int(n/2)
+    # return repeated if n % 2 == 0 else repeated + "수"
