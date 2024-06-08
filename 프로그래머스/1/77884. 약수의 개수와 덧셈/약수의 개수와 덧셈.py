79cfc7bfaaf49576df058e59a3eebafd48d2fe0f
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-06-09 00:04:05 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,16 +1,26 @@
 def solution(left, right):
     sum = 0
     for i in range(left, right + 1):
-        numOfDivisor = getDivisor(i)
-        if (numOfDivisor % 2 == 0):
-            sum += i
-        else:
+        # 약수의 개수가 홀수인 경우는 제곱수인 경우밖에 없음
+        if (i ** 0.5 == int(i ** 0.5)):
             sum -= i
+        else:
+            sum += i
     return sum
 
-def getDivisor(n):
-    numOfDivisor = 0
-    for i in range(1, n + 1):
-        if (n % i == 0):
-            numOfDivisor += 1
-    return numOfDivisor
+# def solution(left, right):
+#     sum = 0
+#     for i in range(left, right + 1):
+#         numOfDivisor = getDivisor(i)
+#         if (numOfDivisor % 2 == 0):
+#             sum += i
+#         else:
+#             sum -= i
+#     return sum
+
+# def getDivisor(n):
+#     numOfDivisor = 0
+#     for i in range(1, n + 1):
+#         if (n % i == 0):
+#             numOfDivisor += 1
+#     return numOfDivisor
