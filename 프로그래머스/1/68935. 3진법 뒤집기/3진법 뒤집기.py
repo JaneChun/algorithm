a97aa76f57dad6795b0c2ee0091a0aabf5e68f52
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-06-09 01:11:24 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,12 +1,11 @@
 def solution(n):
-    n3 = to_base(n, 3)
-    reversed_n3 = str(n3)[::-1]
-    return int(reversed_n3, 3)
-
-def to_base(n, base):
-    result = ""
+    reversedTernary = ""
     while n:
-        result = str(n % base) + result
-        n = n // base # 몫
+        reversedTernary += str(n % 3)
+        n = n // 3
+     
+    return int(reversedTernary, 3)
+
+    
     
     return result
