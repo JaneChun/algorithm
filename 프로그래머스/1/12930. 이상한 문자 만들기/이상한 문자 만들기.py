4544676c3b5ddf765d80e72c6fbe923fd1fd2f41
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-06-09 09:08:04 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,12 +1,20 @@
 def solution(s):
-    result = []
-    for word in s.split(' '):
-        converted_word = ''
-        for i, char in enumerate(word):
-            if i % 2 == 0:
-                converted_word += char.upper() 
-            else: 
-                converted_word += char.lower()
-        result.append(converted_word)
-    return ' '.join(result)
+    return ' '.join(
+        map(lambda word: 
+            ''.join(
+                [char.upper() if i % 2 == 0 else char.lower() for i, char in enumerate(word)]
+            )
+        , s.split(' ')))
+
+
+    # result = []
+    # for word in s.split(' '):
+    #     converted_word = ''
+    #     for i, char in enumerate(word):
+    #         if i % 2 == 0:
+    #             converted_word += char.upper() 
+    #         else: 
+    #             converted_word += char.lower()
+    #     result.append(converted_word)
+    # return ' '.join(result)
             
