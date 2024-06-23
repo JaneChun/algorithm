024825b8d60db9601d1132189388905cd54fb8a6
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-06-23 22:00:01 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,24 @@
-import re
-
 def solution(s):
-    numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(len(numArr)):
-        num = numArr[i]
-        pattern = rf"{num}"
-        match = re.findall(pattern, s)
-        
-        if match:
-            replacedS = re.sub(pattern, str(i), s)
-            s = replacedS
-            
+    numDic = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+    }
+    
+    for word, number in numDic.items():
+        s = s.replace(word, str(number))
     return int(s)
+
+
+# import re
+
+# def solution(s):
+#     numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+#     for i in range(len(numArr)):
+#         num = numArr[i]
+#         pattern = rf"{num}"
+#         match = re.findall(pattern, s)
+        
+#         if match:
+#             replacedS = re.sub(pattern, str(i), s)
+#             s = replacedS
+            
+#     return int(s)
             
