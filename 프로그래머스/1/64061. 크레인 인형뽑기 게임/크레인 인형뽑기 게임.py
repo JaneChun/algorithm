3655734d8b31ef73b1f6c3d1f29352f4601fb90c
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-08-03 09:05:12 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.34 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -0,0 +1,28 @@
+def solution(board, moves):
+    newBoard = []
+    for i in range(len(board)):
+        newBoard.append([])
+    
+    for row in range(len(board) - 1, -1, -1): # 맨 아랫줄부터
+        for col in range(len(board)):
+            item = board[row][col]
+            if item != 0:
+                newBoard[col].append(item)
+    
+    count = 0
+    stack = []
+    for move in moves:
+        if newBoard[move - 1]:
+            stack.append(newBoard[move - 1].pop())
+    
+    
+    count = 0
+    result = []
+    for item in stack:
+        if result and result[-1] == item:
+            result.pop()
+            count += 2
+        else:
+            result.append(item)
+    
+    return count
