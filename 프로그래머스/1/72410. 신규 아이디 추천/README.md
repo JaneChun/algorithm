fe97f90379074f552933693359a4564b44a0e06a
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-08-10 10:18:27 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.30 ms
+메모리: 10.2 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 10일 00:29:45
+2024년 08월 10일 10:18:24
 
 ### 문제 설명
 
--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,14 +1,21 @@
 def solution(new_id):
+    #1
     answer = new_id.lower()
+    #2
     answer = ''.join([x for x in answer if x.isalpha() or x.isdigit() or x in['-', '_', '.']])
+    #3
     while '..' in answer:
         answer = answer.replace('..', '.')
+    #4
     answer = answer.strip('.')
+    #5
     if len(answer) == 0:
         answer = 'a'
+    #6
     if len(answer) >= 16:
         answer = answer[:15]
-    answer = answer.rstrip('.')
+    answer = answer.strip('.')
+    #7
     if len(answer) <= 2:
         char = answer[-1]
         answer += char * (3 - len(answer))
