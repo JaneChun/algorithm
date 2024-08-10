b27fb22202377402e991e339995444212719d929
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-08-10 10:10:28 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.49 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -3,13 +3,13 @@ def solution(data, ext, val_ext, sort_by):
     
     key = ['code', 'date', 'maximum', 'remain']
     mapped_data = [dict(zip(key, value)) for value in data]
-    # [{code:, date: , }, {...}]
     
     for obj in mapped_data:
-        if ext == 'date':
-            if str(obj.get(ext)) < str(val_ext):
-                answer.append(obj)
-        elif obj.get(ext) < val_ext:
+        # if ext == 'date':
+        #     if str(obj.get(ext)) < str(val_ext):
+        #         answer.append(obj)
+        # elif 
+        if obj.get(ext) < val_ext:
             answer.append(obj)
         
     sorted_answer = sorted(answer, key=lambda x:  x.get(sort_by))
