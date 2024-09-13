54098a28ddbb2821fa64db15f035131291d12e11
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-09-13 23:12:37 +0900

    [level 2] Title: 구명보트, Time: 9.33 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    boat_count = 0
+    people.sort()
+    
+    left = 0
+    right = len(people) - 1
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1
+            right -= 1
+            boat_count += 1
+        else:
+            right -= 1
+            boat_count += 1
+        
+    return boat_count 
