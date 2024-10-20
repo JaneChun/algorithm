6e5996a7d1db5ae6b941c585f37e00af5ab8f231
JaneChun <108467211+JaneChun@users.noreply.github.com>
2024-10-20 10:41:47 +0900

    [level 2] Title: 전화번호 목록, Time: 98.73 ms, Memory: 30.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,10 +1,8 @@
 def solution(phone_book):
-    # 먼저 리스트를 정렬
     phone_book.sort()
-    
-    # 정렬된 리스트에서 인접한 전화번호만 비교
     for i in range(len(phone_book) - 1):
-        # 인접한 번호들이 접두어 관계에 있는지 확인
-        if phone_book[i+1].startswith(phone_book[i]):
+        prefix = phone_book[i]
+        
+        if phone_book[i + 1].startswith(prefix):
             return False
     return True
