f0cb562cc197ed817e90657174c880889b1e1ccb
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-01-04 10:27:46 +0900

    [level 2] Title: 배달, Time: 1.84 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 2.11 ms
+메모리: 10.6 MB, 시간: 1.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 29일 00:17:55
+2025년 01월 04일 10:27:44
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -8,21 +8,23 @@ def solution(N, road, K):
             time = min(graph[v1][v2], time)
         graph[v1][v2] = time
         graph[v2][v1] = time
+    print(graph)
         
     # BFS를 사용해 최소 시간 계산
     dist = [float('inf')] * (N + 1)
-    dist[1] = 0
+    dist[1] = 0 # [inf, 0, inf, inf ...]
     
     queue = [(1, 0)] # 현재 마을, 누적 시간
     
     while queue:
         cur, cur_time = queue.pop(0)
         
-        for neighbor, time in graph[cur].items():
-            new_time = cur_time + time
-            if new_time < dist[neighbor]:
+        for neighbor, time in graph[cur].items(): # {2: 1, 4: 2}
+            new_time = cur_time + time # 1
+            if new_time < dist[neighbor]: # 1 < inf
                 dist[neighbor] = new_time # 최단시간 갱신
                 queue.append((neighbor, new_time))
+    print(dist)
                 
     return sum(1 for time in dist if time <= K)
 
