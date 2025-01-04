57a7edc683d9a18c3135cc6132b7d995109a7c56
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-01-04 10:07:36 +0900

    [level 2] Title: 호텔 대실, Time: 3.99 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -4,20 +4,14 @@ def solution(book_time):
     book_time_converted = convert_to_min(book_time)
     book_time_sorted = sorted(book_time_converted, key=lambda x: x[0])
         
-    print(book_time_sorted)
-    # [[900, 1030], [1000, 1110], [860, 930], [850, 1170], [1100, 1290]]
-    
     for start,end in book_time_sorted:
         earliest_available = rooms[0]
         if earliest_available <= start:
             rooms[0] = end
         else:
             rooms.append(end)
-            
-        
         rooms.sort()
-    
-    print(rooms)
+        
     return len(rooms)
 
 def convert_to_min(book_time):
