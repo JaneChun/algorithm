f88b02ef31cba4f12d1d66ce7fa123177df6b0bf
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-02-14 03:32:39 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.36 ms
+메모리: 10.2 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 10일 09:31:26
+2025년 02월 14일 03:32:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.py
@@ -0,0 +1,44 @@
+def solution(places):
+    answer = []
+    for place in places:
+        if is_all_distanced(place):
+            answer.append(1)
+        else:
+            answer.append(0)
+    return answer
+
+def is_all_distanced(place):
+    for row in range(len(place)):
+        for col in range(len(place[row])):
+            if place[row][col] == 'P':
+                # 맨해튼 거리두기를 지고 있는지 확인
+                if is_someone_around(place, [row, col]):
+                    return False
+    return True
+
+# bfs로 확인
+def is_someone_around(place, start):
+    queue = []
+    visited = [[False for j in range(5)] for i in range(5)]
+    
+    queue.append([start, 0]) # [y, x], 누적거리
+    visited[start[0]][start[1]] = True
+    
+    while queue:
+        (y, x), dis = queue.pop(0)
+        
+        if 1<= dis <= 2 and place[y][x] == 'P':
+            return True
+        
+        for dy, dx in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+            ny = y + dy
+            nx = x + dx
+            if (0 <= ny < 5 and 
+                0 <= nx < 5 and 
+                not visited[ny][nx] and 
+                place[ny][nx] != 'X' and 
+                dis < 2
+               ):
+                queue.append([[ny, nx], dis + 1])
+                visited[ny][nx] = True
+    return False
