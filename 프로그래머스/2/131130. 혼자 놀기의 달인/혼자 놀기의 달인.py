54a0a006b5edad3791223ccece9393347c225f5d
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-03-21 23:47:36 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.07 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,25 @@
+def solution(cards):
+    group_sizes = []
+    queue = []
+    visited = [False] * len(cards)
+    cards = list(map(lambda x: x - 1, cards)) # cards는 1 based index이므로 0 based index로 수정해준다.
+    
+    for i in range(len(cards)):        
+        if not visited[i]:
+            queue.append(i) # BFS 시작점 세팅
+            visited[i] = True
+            count = 1
+    
+            while queue:
+                cur_idx = queue.pop(0) # 인덱스 0에 있는
+                cur_box = cards[cur_idx] # 8번 박스
+                if not visited[cur_box]:
+                    queue.append(cur_box)
+                    visited[cur_box] = True
+                    count += 1
+            # BFS 종료 후
+            group_sizes.append(count)
+            
+    group_sizes.sort(reverse = True)
+    
+    return 0 if len(group_sizes) <= 1 else group_sizes[0] * group_sizes[1]
