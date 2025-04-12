dab0dec8db25d71513f2637bcc5f90e7fe3499ad
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-04-12 09:04:16 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -10,23 +10,29 @@ def solution(name):
         answer += min(go_up, go_down)
     
     # 커서 이동 횟수 따로 계산
-    go_straight = len(name) - 1 # 직진하기
+    go_straight = len(name) - 1 # 직진하기(->)
     min_move = go_straight
     
     for i in range(len(name)):
         next_idx = i + 1
+        
         # 다음 문자가 A일 때, 연속된 A가 끝나는 인덱스 찾기
         while next_idx < len(name) and name[next_idx] == 'A':
             next_idx += 1
 
-        right = i # 지금까지 오른쪽으로 이동해온 거리
-        left = len(name) - next_idx # 연속된 A 이후 남은 문자까지의 거리
+        go_back_then_front = (len(name) - next_idx) * 2 + i # 뒤에서 next_idx까지 이동(<-) + 끝까지 처리(->) + i로 돌아옴(->)
+        go_front_then_back = i*2 + len(name) - next_idx # i까지 이동(->) + 앞으로 되돌아감(<-) + 뒤에서 next_idx까지 이동(<-)
         
-        go_back = right + min(right, left) + left
-        # 돌아갈 때, 지금까지 온 방향으로 돌아갈지 반대로 갈지 짧은 쪽 선택
+        min_move = min(min_move, go_back_then_front, go_front_then_back)
         
-        min_move = min(min_move, go_back)
-    
     answer += min_move
 
     return answer
+
+# "ABAAAABAA"
+# i = 1
+# next_idx = 6
+# len(name) = 9
+
+# go_front_then_back = 1*2 + (9 - 6) = 2 + 3 = 5
+# go_back_then_front = (9 - 6)*2 + 1 = 3*2 + 1 = 6 + 1 = 7
