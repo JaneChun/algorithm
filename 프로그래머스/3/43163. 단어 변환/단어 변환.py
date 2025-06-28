50b763cbc1be6eb421ff7b9042130fe3186f3b3b
JaneChun <108467211+JaneChun@users.noreply.github.com>
2025-06-28 22:28:12 +0900

    [level 3] Title: 단어 변환, Time: 0.49 ms, Memory: 9.12 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,25 @@
+def solution(begin, target, words):
+    answer = 0
+    
+    queue = [(begin, 0)]
+    visited = [False] * len(words)
+    
+    while queue:
+        cur, cnt = queue.pop(0)
+        if cur == target:
+            return cnt
+        
+        for i in range(len(words)):
+            if one_letter_different(cur, words[i]) and not visited[i]:
+                queue.append((words[i], cnt + 1))
+                visited[i] = True
+        
+    return answer
+
+def one_letter_different(a, b):
+    count = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count += 1
+            
+    return count == 1
