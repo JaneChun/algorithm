48f4461e48c9c29c497500d2ec76472d87624fb4
JaneChun <108467211+JaneChun@users.noreply.github.com>
2026-03-08 00:33:37 +0900

    [Gold V] Title: 회의실 배정, Time: 280 ms, Memory: 66504 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -7,8 +7,9 @@ for _ in range(n):
     start, end = map(int, sys.stdin.readline().split(" "))
     meetings.append({"start": start, "end": end})
 
-# 끝나는 시간 기준 정렬 -> 회의시간 기준 정렬
+# 끝나는 시간 기준 정렬 -> 시작 시간 기준 정렬
 # 빨리 끝나야 그 뒤로 더 많은 회의를 할 수 있으므로
+# end가 같으면 일찍 시작한 것부터 담아야 연속으로 넣을 수 있음 e.g. (0, 2) -> (2, 2) -> ...
 meetings.sort(key=lambda x: (x["end"], x["start"]))
 
 count = 0
