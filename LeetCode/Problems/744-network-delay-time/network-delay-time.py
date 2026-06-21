8509e45a441e6b9640fd7658c00f89691bc4b20c
JaneChun <108467211+JaneChun@users.noreply.github.com>
2026-06-21 17:46:23 +0900

    Time: 323 ms (95.48%) | Memory: 21.3 MB (38.76%) - LeetSync

--- LeetCode/Problems/744-network-delay-time/network-delay-time.py
+++ LeetCode/Problems/744-network-delay-time/network-delay-time.py
@@ -12,26 +12,22 @@ class Solution:
             graph[source][target] = time
 
         # distance = k로부터 모든 노드까지의 거리
-        distance = [-1] * (n + 1) # 1-based
+        distance = [float('inf')] * (n + 1) # 1-based
 
         # 시작점 초기화
-        q = [(k, 0)]
+        q = [(0, k)] # (거리, 노드번호)로 넣어야 거리 순으로 정렬됨
         distance[k] = 0
         distance[0] = 0
 
         while q:
-            cur, dist = heapq.heappop(q) # 가장 거리가 가까운 노드부터 탐색하도록 최소 힙 사용
+            # 최소힙에서 노드를 꺼내는 순간: 최단거리가 보장된다.
+            dist, cur = heapq.heappop(q) # 가장 거리가 가까운 노드부터 탐색하도록 최소 힙 사용
             
             for nxt, nxt_dist in graph[cur].items():
-                # if distance[nxt] == -1: # 첫 방문이 최소 거리이므로
-                #         heapq.heappush(q, (nxt, dist + nxt_dist))
-                #         distance[nxt] = dist + nxt_dist
-                # X -> 첫 방문이 최소 거리가 아닐 수 있다. 1 -(4)-> 3 보다 1 -(1)-> 2 -(2)->3 이 더 싸다. 
-
-                # 첫 방문인 경우 무조건 탐색하고, 아니면 기존 distance보다 작은 경우에만 탐색
                 acc_dist = dist + nxt_dist
-                if distance[nxt] == -1 or acc_dist < distance[nxt]:
-                    heapq.heappush(q, (nxt, acc_dist))
+                
+                if acc_dist < distance[nxt]: # 처음 발견했거나, 이미 발견했지만 더 짧은 경로를 발견한 경우
+                    heapq.heappush(q, (acc_dist, nxt))
                     distance[nxt] = acc_dist
 
-        return -1 if -1 in distance else max(distance)
+        return -1 if float('inf')in distance else max(filter(lambda x: x != float('inf'), distance))
