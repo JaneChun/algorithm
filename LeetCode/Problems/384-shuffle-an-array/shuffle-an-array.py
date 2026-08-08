37c0f62a9701e8ee2fc9bf68c5f223c29a8b8cbd
JaneChun <108467211+JaneChun@users.noreply.github.com>
2026-08-08 23:11:06 +0900

    Time: 25 ms (38.07%) | Memory: 22.5 MB (66.59%) - LeetSync

--- LeetCode/Problems/384-shuffle-an-array/shuffle-an-array.py
+++ LeetCode/Problems/384-shuffle-an-array/shuffle-an-array.py
@@ -1,5 +1,5 @@
 # shuffle - 배열을 섞어서 만들 수 있는 모든 순서가 동일한 확률로 나와야 한다
-# 예를 들어 num = [1, 2, 3]일 때 가능한 수열은:
+# 예를 들어 nums = [1, 2, 3]일 때 가능한 수열은:
 # [1, 2, 3]
 # [1, 3, 2]
 # [2, 1, 3]
@@ -14,6 +14,7 @@ class Solution:
     def __init__(self, nums: List[int]):
         self.original = nums[:]
         self.current = nums[:]
+        
 
     def reset(self) -> List[int]:
         self.current = self.original[:]
@@ -26,12 +27,22 @@ class Solution:
             # i와 j의 값을 교환한다.
             # 다음 인덱스로 이동한다.
             # 핵심은 랜덤 범위. 이미 확정한 0 ~ i-1 영역은 다시 건드리지 않는다.
+            
+            # 이 방식이 모든 순열을 같은 확률로 나오도록 보장하는 이유는
+            # nums = [1, 2, 3]일 때
+            # i = 0일 때 이 자리에 들어갈 수 있는 요소가 1, 2, 3 -> 1/3 확률
+            # i = 1일 때 이 자리에 들어갈 수 있는 요소가 2, 3    -> 1/2 확률
+            # i = 2일 때 이 자리에 들어갈 수 있는 요소가 3       -> 1/1 확률
+            # 따라서 특정 수열이 나올 수 있는 확률은 1/3 * 1/2 * 1 = 1/6로 동일해진다
         n = len(self.original)
         for i in range(n):
             j = random.randrange(i, n)
             self.current[i], self.current[j] = self.current[j], self.current[i]
-        
+
         return self.current
+
+
+        
         
 
 
